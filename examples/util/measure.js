e244e7bf2d8d581d3e08a4bbdeb49f414f83a7ba
Villem Alango <villem.alango@gmail.com>
2020-01-22 22:27:58 +0200

    made asynchronous: 'examples/util/measure'

--- examples/util/measure.js
+++ examples/util/measure.js
@@ -1,17 +1,17 @@
 /**
- * @module examples/util/duration
+ * @module examples/util/measure
  */
 'use strict'
 
 /**
  * @param {function()} task
- * @returns {number} duration of `task` in microseconds.
+ * @returns {Promise<number>} duration of `task` in microseconds.
  */
 module.exports = (task) => {
   const t0 = process.hrtime()
 
-  task()
-
-  const t1 = process.hrtime(t0)
-  return (t1[0] * 1e9 + t1[1]) / 1000
+  return Promise.resolve(task()).then(() => {
+    const t1 = process.hrtime(t0)
+    return (t1[0] * 1e9 + t1[1]) / 1000
+  })
 }
