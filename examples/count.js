e0b067aba8cb038533d5c9d0299c2f2bfa6996b1
Villem Alango <villem.alango@gmail.com>
2020-01-27 14:34:47 +0200

    comments added to list example

--- examples/count.js
+++ examples/count.js
@@ -9,17 +9,22 @@ const { dump, measure, parseCl, print } = require('./util')
 const counts = {}, { args } = parseCl({}, HELP, true)
 const d = { deepest: '', maxDepth: 0, total: 1 }
 
-//  =======  Start of application-specific code.
+//  =======  Start of application-specific code.      =======
 
 const add = (key) => {
   if (!counts[key]) counts[key] = 0
   ++counts[key] && ++d.total
 }
 
+//  Use plugin strategy to avoid class declaration.
+//  In general case, we should call Walker instance method,
+//  but do not do it here.
+
 const onBegin = ({ absDir, depth }) => {
   if (depth <= d.maxDepth) return
   (d.deepest = absDir) && (d.maxDepth = depth)
 }
+
 const onEntry = ({ type }) => add(type)
 
 const tick = () => process.stdout.write(d.total + '\r')
@@ -29,7 +34,7 @@ const walker = new Walker({ onBegin, onEntry, tick })
 measure(
   () => args.map((dir) => walker.walkSync(dir)) && {}
 ).then(({ time }) => {
-//  =======  Start of boilerplate code for reporting.
+//  =======  Start of boilerplate code for reporting. =======
   dump(walker.failures, color.redBright,
     'Total %i failures.', walker.failures.length)
 
